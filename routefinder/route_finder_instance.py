b7a1b677da485b258def399c36485dc73a5df272
Eric S. Talbot <est1209@gmail.com>
2015-05-08 13:57:02 -0400

    use django settings to specify graph file path

--- routefinder/route_finder_instance.py
+++ routefinder/route_finder_instance.py
@@ -1,9 +1,12 @@
+from django.conf import settings
+
 from routefinder import MidLinkRouteFinder
 from weight_presets import weight_presets
 
 import cPickle
 
-multigraph, crs = cPickle.load(open('multigraph.pickle'))
+path = settings.ROUTEFINDER['graph_path']
 
+multigraph, crs = cPickle.load(open(path))
 
 rf = MidLinkRouteFinder(multigraph, weight_presets)
