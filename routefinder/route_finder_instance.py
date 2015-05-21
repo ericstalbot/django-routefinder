5291e004e5d95be804bc264b26d5638f9cbc1ff0
Eric S. Talbot <est1209@gmail.com>
2015-05-20 20:52:41 -0400

    decouple vermont-specific data

--- routefinder/route_finder_instance.py
+++ routefinder/route_finder_instance.py
@@ -1,12 +1,11 @@
 from django.conf import settings
 
 from routefinder import MidLinkRouteFinder
-from weight_presets import weight_presets
 
 import cPickle
 
 path = settings.ROUTEFINDER['graph_path']
 
-multigraph, crs = cPickle.load(open(path))
+multigraph, weight_presets, crs = cPickle.load(open(path))
 
 rf = MidLinkRouteFinder(multigraph, weight_presets)
